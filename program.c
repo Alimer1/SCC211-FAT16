e803567d3cac6095180877264263c73f9569426b
Alimer1 <aliomermsrl@gmail.com>
2022-12-02 04:21:28 +0000

    Finished Task4. Organized the code a bit.

--- program.c
+++ program.c
@@ -30,19 +30,19 @@ typedef struct __attribute__((__packed__))
 
 typedef struct __attribute__((__packed__))
 {
-    uint8_t DIR_Name[ 11 ];     // Non zero terminated string
-    uint8_t DIR_Attr;           // File attributes
-    uint8_t DIR_NTRes;          // Used by Windows NT, ignore
-    uint8_t DIR_CrtTimeTenth;   // Tenths of sec. 0...199
-    uint16_t DIR_CrtTime;       // Creation Time in 2s intervals
-    uint16_t DIR_CrtDate;       // Date file created
-    uint16_t DIR_LstAccDate;    // Date of last read or write
-    uint16_t DIR_FstClusHI;     // Top 16 bits file's 1st cluster
-    uint16_t DIR_WrtTime;       // Time of last write
-    uint16_t DIR_WrtDate;       // Date of last write
-    uint16_t DIR_FstClusLO;     // Lower 16 bits file's 1st cluster
-    uint32_t DIR_FileSize;      // File size in bytes
-} File;
+	uint8_t		DIR_Name[ 11 ];		// Non zero terminated string
+	uint8_t		DIR_Attr;           // File attributes
+	uint8_t		DIR_NTRes;          // Used by Windows NT, ignore
+	uint8_t		DIR_CrtTimeTenth;   // Tenths of sec. 0..199
+	uint16_t	DIR_CrtTime;        // Creation Time in 2s intervals
+	uint16_t	DIR_CrtDate;
+	uint16_t	DIR_LstAccDate;     // Date of last read or write
+	uint16_t	DIR_FstClusHI;      // Top bits file's 1st cluster
+	uint16_t	DIR_WrtTime;        // Time of last write
+	uint16_t	DIR_WrtDate;        // Date of last write
+	uint16_t	DIR_FstClusLO;      // Lower bits file's 1st cluster
+	uint32_t	DIR_FileSize;       // File size in bytes
+} DirectoryEntry;
 
 typedef struct LinkedListElement
 {
@@ -50,10 +50,87 @@ typedef struct LinkedListElement
     struct LinkedListElement *nextElement;
 } LinkedListElement;
 
-BootSector getBootSector(int fd)
+typedef struct Date
+{
+    int day;
+    int month;
+    int year;
+} Date;
+
+typedef struct Time
+{
+    int seconds;
+    int minutes;
+    int hours;
+} Time;
+
+Date getDateInfo(uint16_t date)
+{
+    Date resultDate;
+    resultDate.day = 0;
+    resultDate.month = 0;
+    resultDate.year = 1980;
+
+    resultDate.day += ((date >> 0) & 0x1F);     // 00011111
+    resultDate.month += ((date >> 5) & 0x0F);   // 00001111
+    resultDate.year += ((date >> 9) & 0x7F);    // 01111111
+
+    return(resultDate);
+}
+
+Time getTimeInfo(uint16_t time)
+{
+    Time resultTime;
+    resultTime.seconds = 0;
+    resultTime.minutes = 0;
+    resultTime.hours = 0;
+
+    resultTime.seconds += ((time >> 0) & 0x1F); // 00011111
+    resultTime.minutes += ((time >> 5) & 0x3F); // 00111111
+    resultTime.hours += ((time >> 11) & 0x1F);  // 00011111
+
+    resultTime.seconds *= 2;
+
+    return(resultTime);
+}
+
+//Prints a the given directory entries properties in a row
+void printDirectoryEntry(DirectoryEntry givenDE)
+{
+    Time writeTime = getTimeInfo(givenDE.DIR_WrtTime);
+    Date writeDate = getDateInfo(givenDE.DIR_WrtDate);
+    printf("    ");
+    for(int i=0;i<11;i++)
+    {
+        printf("%c",givenDE.DIR_Name[i]);
+    }
+    printf("    ");
+    if((givenDE.DIR_Attr >> 0) & 0x01 == 1) printf("R");
+    else(printf("-"));
+    if((givenDE.DIR_Attr >> 1) & 0x01 == 1) printf("H");
+    else(printf("-"));
+    if((givenDE.DIR_Attr >> 2) & 0x01 == 1) printf("S");
+    else(printf("-"));
+    if((givenDE.DIR_Attr >> 3) & 0x01 == 1) printf("V");
+    else(printf("-"));
+    if((givenDE.DIR_Attr >> 4) & 0x01 == 1) printf("D");
+    else(printf("-"));
+    if((givenDE.DIR_Attr >> 5) & 0x01 == 1) printf("A");
+    else(printf("-"));
+    printf("    ");
+    printf("%02d:%02d:%02d",writeTime.hours,writeTime.minutes,writeTime.seconds);
+    printf("    ");
+    printf("%02d/%02d/%04d",writeDate.day,writeDate.month,writeDate.year);
+    printf("    ");
+    printf("%04X %04X",givenDE.DIR_FstClusHI,givenDE.DIR_FstClusLO);
+    printf("    ");
+    printf("%i bytes\n",givenDE.DIR_FileSize);
+}
+
+BootSector readBootSector(int fd)
 {
     BootSector bootSector;
-    BootSector *buffer = (BootSector*)malloc(sizeof(BootSector));
+    BootSector* buffer = malloc(sizeof(BootSector));
     read(fd,buffer,sizeof(BootSector));
     bootSector = *buffer;
     free(buffer);
@@ -98,12 +175,34 @@ void printList(struct LinkedListElement *head)
 
 int main()
 {
-    //printf("Hello World\n");
-
+    //Openning the Volume
     int fd = open("fat16.img",O_RDONLY);
 
-    BootSector bootSector = getBootSector(fd);
+    //Reading the BootSector
+    BootSector bootSector = readBootSector(fd);
 
+    //FAT OffSet and Size Calculation
+    int fATOffSet = bootSector.BPB_BytsPerSec * bootSector.BPB_RsvdSecCnt;
+    int fATSectionSize = bootSector.BPB_BytsPerSec * bootSector.BPB_FATSz16;
+
+    //RootDirectory OffSet and Size Calculation
+    int rootDirectoryOffSet = fATOffSet + (fATSectionSize * bootSector.BPB_NumFATs);
+    int rootDirectorySize = bootSector.BPB_RootEntCnt * sizeof(DirectoryEntry);
+
+    //Data OffSet Calculation
+    int dataOffSet = rootDirectoryOffSet + rootDirectorySize;
+
+    //Reading FAT[0]
+    lseek(fd,fATOffSet,SEEK_SET);
+    int16_t* firstFAT = malloc(fATSectionSize);
+    read(fd,firstFAT,fATSectionSize);
+
+    //Reading the RootDirectory
+    lseek(fd,rootDirectoryOffSet,SEEK_SET);
+    DirectoryEntry *rootDirectory = malloc(rootDirectorySize);
+    read(fd,rootDirectory,rootDirectorySize);
+
+    //Printing the Important BootSector Values
     printf("BPB_BytsPerSec: %i\n",  bootSector.BPB_BytsPerSec);
     printf("BPB_SecPerClus: %i\n",  bootSector.BPB_SecPerClus);
     printf("BPB_RsvdSecCnt: %i\n",  bootSector.BPB_RsvdSecCnt);
@@ -112,44 +211,39 @@ int main()
     printf("BPB_TotSec16: %i\n",    bootSector.BPB_TotSec16);
     printf("BPB_FATSz16: %i\n",     bootSector.BPB_FATSz16);
     printf("BPB_TotSec32: %i\n",    bootSector.BPB_TotSec32);
-
-    for(int i=0;i<11;i++)
+    printf("BS_VolLab: {%i",        bootSector.BS_VolLab[0]);
+    for(int i=1;i<11;i++)
     {
-        printf("BS_VolLab[%i]: %i\n",i,bootSector.BS_VolLab[i]);
+        printf(",%i",bootSector.BS_VolLab[i]);
+    }
+    printf("}\n\n");
+
+    //Printing the Root Directory
+    printf("    FILE-NAME      ATTR      WRT-TIME    WRT-DATE      FIRST-CLSTR  FILE-SIZE\n");
+    for(int i=0;i<bootSector.BPB_RootEntCnt;i++)
+    {
+        DirectoryEntry currentDE = rootDirectory[i];
+        if(
+            !(currentDE.DIR_Name[0] == 0x00 ||currentDE.DIR_Name[0] == 0xE5)                             //Don't print in case the first byte of the name is 0x00 or 0xE5
+            &&
+            !(((currentDE.DIR_Attr >> 0) & 0x0F) == 0x0F && ((currentDE.DIR_Attr >> 4) & 0x03) == 0x00)  //Don't print if the first 4 bits of the attribute are 1 and last 2 bits of the attribute are 0
+        )
+        {
+            printDirectoryEntry(currentDE);
+        }
+
     }
 
-    int fATOffSet = bootSector.BPB_BytsPerSec * bootSector.BPB_RsvdSecCnt;
-    int fATSectionSize = bootSector.BPB_BytsPerSec * bootSector.BPB_FATSz16;
-
-    lseek(fd,fATOffSet,SEEK_SET);
-    int16_t* firstFAT = (int16_t*)malloc(fATSectionSize);
-    read(fd,firstFAT,fATSectionSize);
-
-    int rootDirectoryOffSet = bootSector.BPB_BytsPerSec * (bootSector.BPB_RsvdSecCnt + (bootSector.BPB_NumFATs * bootSector.BPB_FATSz16));
-    int rootDirectorySize = bootSector.BPB_BytsPerSec * sizeof(File);
-
-    lseek(fd,rootDirectoryOffSet,SEEK_SET);
-    File* rootDirectory = (File*)malloc(sizeof(rootDirectorySize));
-    read(fd,rootDirectory,rootDirectorySize);
-
-    printf("First File Name:");
-    for(int i=0;i<11;i++)
-    {
-        printf("%c",rootDirectory[0].DIR_Name[i]);
-    }
-    printf("\n");
-
+    //Too much to display I think everything is correct.
     /*
-
-    Too much to display I think everything is correct.
-
     for(int i=0;i<600;i++)
     {
         printf("FAT Array[%i]:%i\n",i,firstFAT[i]);
     }
     */
-    
 
+    //Linked List Test
+    /*
     struct LinkedListElement *head = NULL;
     head = malloc(sizeof(struct LinkedListElement));
 
@@ -164,8 +258,8 @@ int main()
     }
     printList(head);
     freeList(head);
+    */
 
-    printf("Size of a file???: %li\n",sizeof(File));
     free(rootDirectory);
     free(firstFAT);
     close(fd);
